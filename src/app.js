2e26bebbdffc6d70dd0bb1b524aa4e329abd90e2
Américo Valdazo <63754514+ValdazoAmerico@users.noreply.github.com>
2024-10-08 16:56:30 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -71,7 +71,7 @@ const flow = addKeyword([REGEX_ANY_CHARACTER], { regex: true }) // Wrap the rege
             
             // Check if outputMessage is defined before trying to use it
             if (outputMessage) {
-                await flowDynamic(`${JSON.stringify(outputMessage)}`); // Send the API response back to the user
+                await flowDynamic(`${JSON.stringify(outputMessage).replace(/\"/g, '').replace(/\n/g, ' ')}`);
             } else {
                 await flowDynamic('No response received from the chatbot.');
             }
